40fdfe7d22d5c9d56ab3447acabe7fa227e684e2
Mandeep Singh Panwar <fit.panwar@gmail.com>
2020-06-03 21:04:31 +0530

    added compose route with input

--- app.js
+++ app.js
@@ -30,6 +30,14 @@ app.get("/contact", (req, res) => {
   res.render("contact", { content: contactContent });
 });
 
+app.get("/compose", (req, res) => {
+  res.render("compose");
+});
+app.post("/compose", (req, res) => {
+  console.log(req.body.blogInput);
+  res.redirect("/compose");
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
