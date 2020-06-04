cea10a25686fb6cec3ca494f138a7950abc57eb7
Mandeep Singh Panwar <fit.panwar@gmail.com>
2020-06-04 14:27:40 +0530

    fully working

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -18,8 +19,10 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+let posts = [];
+
 app.get("/", (req, res) => {
-  res.render("home", { content: homeStartingContent });
+  res.render("home", { content: homeStartingContent, posts: posts });
 });
 
 app.get("/about", (req, res) => {
@@ -33,9 +36,28 @@ app.get("/contact", (req, res) => {
 app.get("/compose", (req, res) => {
   res.render("compose");
 });
+
+app.get("/posts/:postName", (req, res) => {
+  const name = _.lowerCase(req.params.postName);
+
+  posts.forEach((i) => {
+    title = _.lowerCase(i.title);
+
+    if (title === name) {
+      res.render("post", { post: i });
+    } else {
+      console.log("No match");
+    }
+  });
+});
+
 app.post("/compose", (req, res) => {
-  console.log(req.body.blogInput);
-  res.redirect("/compose");
+  const post = {
+    title: req.body.title,
+    post: req.body.post,
+  };
+  posts.push(post);
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
